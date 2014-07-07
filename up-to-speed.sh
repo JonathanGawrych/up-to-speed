9ba7f228f5420831848c5e05fc623551f9d34989
Trevor Fenn <Trevor.E.Fenn@gmail.com>
2014-07-07 12:52:33 -0600

    Make atom and tomcat conditional

--- up-to-speed.sh
+++ up-to-speed.sh
@@ -18,12 +18,42 @@ echo ""
 echo "Please enter your email: "
 read email
 
+echo ""
+echo "Would you like to install tomcat? (Y (default)/N)"
+read tomcat
+
+if [ -z "$tomcat" ]
+	then tomcat="Y"
+fi
+
+echo ""
+echo "Do you want Sublime, Atom, Both, or Neither? (S/A/B (default)/N)"
+read editor
+
+if [ -z "$editor" ]
+	then editor="B"
+fi
+
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-sudo add-apt-repository -y ppa:webupd8team/atom
+if [ "$editor" == "S" ] || [ "$editor" == "B" ] || [ "$editor" == "s" ] || [ "$editor" == "b" ]
+	then sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+fi
+if [ "$editor" == "A" ] || [ "$editor" == "B" ] || [ "$editor" == "a" ] || [ "$editor" == "b" ]
+	then sudo add-apt-repository -y ppa:webupd8team/atom
+fi
 sudo apt-get update
-sudo apt-get install -y nautilus-open-terminal light-themes dmz-cursor-theme openjdk-7-jdk sublime-text-installer git meld eclipse maven gparted curl atom google-chrome-stable vim tomcat7
+sudo apt-get install -y nautilus-open-terminal light-themes dmz-cursor-theme openjdk-7-jdk sublime-text-installer git meld eclipse maven gparted curl atom google-chrome-stable vim
+
+if [ "$editor" == "S" ] || [ "$editor" == "B" ] || [ "$editor" == "s" ] || [ "$editor" == "b" ]
+	then sudo apt-get install -y sublime-text-installer
+fi
+if [ "$editor" == "A" ] || [ "$editor" == "B" ] || [ "$editor" == "a" ] || [ "$editor" == "b" ]
+	then sudo apt-get install -y atom
+fi
+if [ "$tomcat" == "Y" ] || [ "$tomcat" == "y" ]
+	then sudo apt-get install -y tomcat7
+fi
 
 # We want to install nodejs by n. n is installed by npm. npm is installed by nodejs.
 # To resolve this cirular dependency, we use apt as the entryway, then run around in a circle
@@ -43,7 +73,10 @@ source ~/.bashrc
 npm install -g --prefix=$(npm config get prefix) less jshint@2.4.4 yo bower grunt generator-angular
 
 # Atom packes
-apm install color-picker linter linter-jshint css-color-highlight emmet editorconfig file-icons minimap minimap-git-diff minimap-color-hilight image-view git-difftool
+if [ "$editor" == "A" ] || [ "$editor" == "B" ] || [ "$editor" == "a" ] || [ "$editor" == "b" ]
+	then apm install color-picker linter linter-jshint css-color-highlight emmet editorconfig file-icons minimap minimap-git-diff minimap-color-hilight image-view git-difftool
+fi
+
 # If they clone the repo, copy it. If they just downloaded the script, attempt to grab it from github.
 [ -f .jshintrc ] && cp .jshintrc ~ || wget https://raw.githubusercontent.com/JonathanGawrych/Linux-up-to-speed/master/.jshintrc -P ~
 wget https://sublime.wbond.net/Package%20Control.sublime-package -P ~/.config/sublime-text-3/Installed\ Packages
