c6af93dbf705926724257187e0ccd381a673399f
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2014-08-29 10:47:35 -0600

    feat(apm): add sublime-tabs and tabs-to-spaces to atom

--- up-to-speed.sh
+++ up-to-speed.sh
@@ -89,7 +89,7 @@ npm install -g --prefix=$(npm config get prefix) less jshint@2.4.4 yo bower grun
 
 # Atom packes
 if [ "$editor" == "A" ] || [ "$editor" == "B" ] || [ "$editor" == "a" ] || [ "$editor" == "b" ]
-	then apm install atom-color-highlight atom-jshint autocomplete-paths autocomplete-plus color-picker editorconfig enhanced-package-list file-icons git-difftool minimap minimap-color-highlight minimap-find-and-replace minimap-git-diff minimap-highlight-selected minimap-selection monokai project-palette-finder
+	then apm install atom-color-highlight atom-jshint autocomplete-paths autocomplete-plus color-picker editorconfig enhanced-package-list file-icons git-difftool minimap minimap-color-highlight minimap-find-and-replace minimap-git-diff minimap-highlight-selected minimap-selection monokai project-palette-finder sublime-tabs tabs-to-spaces
 	echo -e "\n'.workspace .editor:not(.mini)':\n  'ctrl-shift-L': 'editor:split-selections-into-lines'" >> ~/.atom/keymap.cson
 fi
 
